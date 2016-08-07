c7bdb818e434d869a4d600d480d969785d038288
devsh <digimon.1740@gmail.com>
2016-08-07 22:21:52 +0900

    챕터4 연습문제 코드 작성

---
+++ chapter4/practice.py
@@ -0,0 +1,42 @@
+print(123 + 456)
+
+print('123' + '456')
+
+# 다음 코드 중 잘못된 것을 고르세요.
+print(123 + 456)
+print(1.23 / 4.56)
+# print('123' * '456')
+
+a = 10
+a -= 5
+print(a)
+
+a = 'Good Morning'
+print(a[:4])
+
+print(len('안녕하세요'))
+
+print(4 >> 2)
+
+print(4 << 2)
+
+print(4 | 3)
+
+print(4 & 3)
+
+x = 0b1001
+print(x)
+
+x = 0xFF
+print(x)
+
+x = 0xF0 | 0x0F
+print(x)
+
+a = '{0} {1}'.format('Mario', 40)
+b = '{name} {age}'.format(name='Luigi', age=35)
+
+print(a)
+print(b)
+
+

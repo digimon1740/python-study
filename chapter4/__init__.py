957ced376abdbf1ced8c5ea3ac92c959146f0ed4
Umtaeho <iwon06@naver.com>
2016-08-08 12:59:18 +0900

    chapter4 commit

--- chapter4/__init__.py
+++ chapter4/__init__.py
@@ -1,1 +1,45 @@
-print('hello world')
+print('hello python')
+
+a = 123
+b = 456
+
+print(a+b)
+
+print(123+456)
+
+print('123' + '456')
+
+print(123+456)
+print(1.23 / 4.56)
+# print('123' * '456') 잘못된것
+
+a = 10
+a -= 5
+print(a)
+
+a= 'Good Moring'
+print(a[:4])
+
+print(len('안녕하세요'))
+
+print(4 >> 2)
+
+print(4 << 2)
+
+print(4 | 3)
+
+print(4 & 3)
+
+x = 0b1001
+print(x)
+
+x = 0xFF
+print(x)
+
+x = 0xF0 | 0x0F
+print(x)
+
+a = '{0} {1}'.format('Mario' , 40)
+print(a)
+b = '{name} {age}'.format(name = 'Luigi' , age = 35)
+print(b)

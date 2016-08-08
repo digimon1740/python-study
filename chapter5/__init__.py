b34aecd04e94c46670e5b488ac229ce0a59f2d06
Umtaeho <iwon06@naver.com>
2016-08-08 12:59:32 +0900

    chapter5 commit

---
+++ chapter5/__init__.py
@@ -0,0 +1,20 @@
+a = [1, 2, 3]
+a.append(4)
+print (a)
+
+a = [1, 2, 3, 4, 5]
+a.pop(2)
+print(a)
+
+city, latitude, longitude = 'Seoul', 37.541, 126.986
+
+print(city)
+print(latitude)
+print(longitude)
+
+a = (1, 2, 3)
+# a[0] = 7
+print(a[0])
+
+dic = {'애플' : 'www.apple.com', '파이썬' : 'www.python', '마이크로소프트' : 'www.microsoft.com'}
+print(dic)

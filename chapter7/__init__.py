6edb384f602681b97088fb6542c9f60709307bb5
Umtaeho <iwon06@naver.com>
2016-08-17 15:34:50 +0900

    chapter7 commit

---
+++ chapter7/__init__.py
@@ -0,0 +1,16 @@
+def min(a,b):
+    result = a
+
+    if ( a > b ):
+        result = b
+
+    return result
+
+def my_abs(arg):
+    if (arg < 0):
+        result = agr * -1
+
+    else:
+        result = agr
+
+    return result

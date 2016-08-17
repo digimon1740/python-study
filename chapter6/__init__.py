72867221954f87b3282d61d785041a3f4f691ebe
Umtaeho <iwon06@naver.com>
2016-08-17 15:13:31 +0900

    chapter6 commit

--- chapter6/__init__.py
+++ chapter6/__init__.py
@@ -10,10 +10,60 @@ for i in range(1,6) :
 
     print()
 
-for i in range(6 , 1, 1) :
+for i in range(5,0,-1) :
     for j in range(i):
 
-        print("*" , end = "" , )
+        print("*" , end = "",)
 
     print()
+
+i = 0
+j = 0
+while i <= 4:
+
+    i = i+1
+    j = 0
+
+    while j < i:
+
+        j = j + 1
+
+        print("*" , end="")
+
+    print()
+
+i = 6
+j = 0
+while i >= 1:
+
+    i = i-1
+    j = 0
+
+    while j < i:
+
+        j = j + 1
+
+        print("*" , end="")
+
+    print()
+
+while True:
+
+    print("반복할 횟수를 입력하세요 : ")
+
+    num = int(input())
+
+    if num <= 0:
+
+        print("0보다 작거나 같은수는 입력할 수 없습니다.")
+
+    else:
+        for i in range (1, num+1):
+            for j in range(i):
+
+                print("*", end="")
+
+            print()
+
+
 
